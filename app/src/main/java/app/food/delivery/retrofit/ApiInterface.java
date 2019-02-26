aad1ae979029050000c6e13a01415ca8b4706b53
keshu <keshuvodedara@gmail.com>
2019-02-26 09:59:44 +0530

    keshu

---
+++ app/src/main/java/app/food/delivery/retrofit/ApiInterface.java
@@ -0,0 +1,9 @@
+package app.food.delivery.retrofit;
+
+
+public interface ApiInterface {
+  /*
+    @FormUrlEncoded
+    @POST("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Field("api_key") String apiKey);*/
+}
