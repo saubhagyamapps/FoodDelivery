9f966f01057f4c8287545701838b16a9878c1c84
Nidhi Patel <np259233@gmail.com>
2019-02-27 18:08:19 +0530

    Nidhi

---
+++ app/src/main/java/app/food/delivery/fragment/ProfileFragment.java
@@ -0,0 +1,19 @@
+package app.food.delivery.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import app.food.delivery.R;
+
+public class ProfileFragment extends Fragment {
+    View mView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_profile, container, false);
+        return mView;
+    }
+}
